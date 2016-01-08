30dd9f8a444269062c3b7960a9dfc2d0434f001f
Cristian Romo <cristian.g.romo@gmail.com>
2016-01-07 22:27:32 -0600

    Add luafun

--- image/image.lua
+++ image/image.lua
@@ -1,4 +1,14 @@
+delight = require 'delight'
+require 'fun' ()
+
+love.load = function()
+   id = love.image.newImageData(8, 8)
+   range(0, 7):each(function(v) id:setPixel(v, 5, 255, 0, 0, 255) end)
+   id:setPixel(5, 5, 255, 0, 0, 255)
+   i = love.graphics.newImage(id)
+end
 
 love.draw = function()
   love.graphics.print("Hello World!", 100, 100)
+  love.graphics.draw(i)
 end
