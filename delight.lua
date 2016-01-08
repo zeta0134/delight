5fb361700eb161c59a04dd3c79d078504f3a7a80
Cristian Romo <cristian.g.romo@gmail.com>
2016-01-07 20:55:46 -0600

    Change 3 space indent to 2 space indent

--- delight.lua
+++ delight.lua
@@ -1,37 +1,37 @@
 local delight = {}
 
 function delight.newImage(filename)
-   local image_data, error = delight.newImageData(filename)
-   if image_data then return love.graphics.newImage(image_data) end
-   return nil, error
+  local image_data, error = delight.newImageData(filename)
+  if image_data then return love.graphics.newImage(image_data) end
+  return nil, error
 end
 
 function delight.newImageData(filename)
-   local file_data, error = delight.newFileData(filename)
-   if file_data then return love.image.newImageData(file_data) end
-   return nil, error
+  local file_data, error = delight.newFileData(filename)
+  if file_data then return love.image.newImageData(file_data) end
+  return nil, error
 end
 
 function delight.newFileData(filename)
-   local file, error = io.open(filename, 'rb')
-   if file then
-      local file_data = love.filesystem.newFileData(file:read('*a'), filename)
-      file:close()
-      return file_data
-   end
-   return nil, error
+  local file, error = io.open(filename, 'rb')
+  if file then
+    local file_data = love.filesystem.newFileData(file:read('*a'), filename)
+    file:close()
+    return file_data
+  end
+  return nil, error
 end
 
 function delight.writeImage(object, filename)
-   if object:typeOf('Image') then object = object:getData() end
-   if object:typeOf('ImageData') then object = object:encode('png') end
-   if object:typeOf('FileData') then object = object:getString() end
-   local file, error = io.open(filename, 'wb')
-   if file then
-      file:write(object)
-      file:close()
-   end
-   return error
+  if object:typeOf('Image') then object = object:getData() end
+  if object:typeOf('ImageData') then object = object:encode('png') end
+  if object:typeOf('FileData') then object = object:getString() end
+  local file, error = io.open(filename, 'wb')
+  if file then
+    file:write(object)
+    file:close()
+  end
+  return error
 end
 
 return delight
